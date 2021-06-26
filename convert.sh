0ddef087504fc8491ccd7b10daea028586150d22
Akira Ohgaki <akiraohgaki@gmail.com>
2021-06-26 23:13:01 +0900

    Format

--- convert.sh
+++ convert.sh
@@ -17,22 +17,19 @@ medium_geometry=22
 large_geometry=48
 
 # Change current working directory
-cd `dirname $0`
+cd $(dirname $0)
 
 # Convert to PNG
-for geometry in $small_geometry $medium_geometry $large_geometry
-do
-    for sub_dir in $source_icons_dir/*
-    do
-        sub_dir=`basename $sub_dir`
-        export_dir=$original_color_icons_dir/$geometry/$sub_dir
-        mkdir -p $export_dir
-        for import_file in $source_icons_dir/$sub_dir/*.svg
-        do
-            export_file=$export_dir/`basename $import_file .svg`'.png'
-            inkscape -z -f $import_file -e $export_file -w $geometry -h $geometry
-        done
+for geometry in $small_geometry $medium_geometry $large_geometry; do
+  for sub_dir in $source_icons_dir/*; do
+    sub_dir=$(basename $sub_dir)
+    export_dir=$original_color_icons_dir/$geometry/$sub_dir
+    mkdir -p $export_dir
+    for import_file in $source_icons_dir/$sub_dir/*.svg; do
+      export_file=$export_dir/$(basename $import_file .svg).png
+      inkscape -z -f $import_file -e $export_file -w $geometry -h $geometry
     done
+  done
 done
 
 # Convert to negative color
